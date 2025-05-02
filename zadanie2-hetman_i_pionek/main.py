d60f87c4219851eea3f6a21c4b5b3ef3adfe0265
Agnieszzka24 <124368425+Agnieszzka24@users.noreply.github.com>
2025-05-02 17:22:53 +0200

    feat: add random queen placement functionality

--- zadanie2-hetman_i_pionek/main.py
+++ zadanie2-hetman_i_pionek/main.py
@@ -1,14 +1,22 @@
 from generate_board import GenerateBoard
+import random
+
+
+def get_random_queens_number():
+    """Funkcja do losowania liczby hetmanów"""
+    return random.randint(1, 5)
 
 if __name__ == "__main__":
     # Wyświelenie planszy
     board = GenerateBoard()
 
-    board.add_random_queen(3)
+    queens_number = get_random_queens_number()
+
+    board.add_random_queen(queens_number)
     board.add_random_pawn()
 
     print("\n Plansza")
     board.display_board()
 
     print("\n Pozycje hetmanów:", [board.chess_notation(x,y) for x, y in board.queens])
-    print("\n Pozycja pionka:", board.chess_notation(*board.pawn_positions))
+    print(" Pozycja pionka:", board.chess_notation(*board.pawn_positions))
