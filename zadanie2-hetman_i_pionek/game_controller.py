698ceaee08bb7cb72b50f26ec3436c05943ae589
Agnieszzka24 <agnieszka1mazur@outlook.com>
2025-05-05 18:47:52 +0200

    feat: refactor game controller to improve game flow and user interaction

--- zadanie2-hetman_i_pionek/game_controller.py
+++ zadanie2-hetman_i_pionek/game_controller.py
@@ -1,123 +1,122 @@
-from generate_board import GenerateBoard
+import random
 from display_board import DisplayBoard
 from chess_logic import ChessLogic
+from generate_board import GenerateBoard
 
-import random
 
 class GameController:
     def __init__(self):
+        """Inicjalizacja kontrolera gry"""
         self.board = GenerateBoard()
-        self.display= DisplayBoard()
-        self.initialize_game()
+        self.display = DisplayBoard()
 
 
 
-    def initialize_game(self):
-        """Inicializacja nowej gry"""
+    def start_game(self):
+        """Główna metoda uruchamiająca grę"""
+        self._initialize_game()
+        self.run()
+
+
+
+    def _initialize_game(self):
+        """Inicjalizacja nowej gry z losową liczbą hetmanów"""
         queens_number = random.randint(1, 5)
         self.board.add_random_queen(queens_number)
         self.board.add_random_pawn()
 
 
 
-    def get_user_choice(self):
-        """Wyświtelnie menu i pobranie wyboru użytkownika"""
-        print("\n Wybierz opcję:")
-        print(" 1. Wylosuj nową pozycję dla pionka")
-        print(" 2. Usunięcie dowolnego hetmana")
-        print(" 3. Sprawdź ponownie bicie")
-        print(" 4. Zakończ grę")
-
-        while True:
-            choice = input(" Twój wybór(1-4): ")
-            if choice in ['1', '2', '3', '4']:
-                return int(choice)
-            print(" Niepoprawny wybór, spróbuj ponownie.")
-
-
-
-    def move_pawn(self):
-        """1. Losowanie nowej pozycji dla pionka"""
-        old_x, old_y = self.board.pawn_positions
-        self.board.board[old_x][old_y] = '░'
-        self.board.add_random_pawn()
-        print("\n Nowa pozycja pionka:", self.board.chess_notation(*self.board.pawn_positions))
-
-
-
-    def remove_queen(self):
-        """2. Usuwa wybranego przez użytkownika hetmana"""
-        if not self.board.queens:
-            print(" Nie ma hetmanów do usunięcia.")
-            return False
-
-        print("\n Dostępne hetmany do usunięcia:")
-        for i, (x, y) in enumerate(self.board.queens):
-            print(f" {i + 1}. {self.board.chess_notation(x, y)}")
-
-        while True:
-            try:
-                choice = int(input(" Wybierz hetmana do usunięcia lub zero aby anulować: "))
-                if choice == 0:
-                    return False
-                if 1 <= choice <= len(self.board.queens):
-                    x, y = self.board.queens[choice - 1]
-                    self.board.board[x][y] = '░'
-                    del self.board.queens[choice - 1]
-                    print(f" Usunięto hetmana z pozycji: {self.board.chess_notation(x, y)}")
-                    return True
-                print(" Niepoprawny wybór, spróbuj ponownie.")
-            except ValueError:
-                print(" Spróbuj jeszcze raz wprowadzić liczbę.")
-
-
-
-    def check_pawn_threat(self):
-        """3. Sprawdza czy pionek jest zagrożony"""
-        threading_queens = ChessLogic.find_threatening_queens(self.board.queens, self.board.pawn_positions)
-        if threading_queens:
-            print("\n Pionek może być zbity przez:")
-            for queen in threading_queens:
-                print(f" - {self.board.chess_notation(*queen)}")
-        else:
-            print("\n Pionek jest bezpieczny")
-
-
-
-    def display_current_state(self):
-        """Wyświetla aktualny stan planszy"""
-        print("\n Plansza")
-        self.display.display_board(self.board.board, self.board.queens, self.board.pawn_positions)
-        print("\n Pozycje hetmanów:", [self.board.chess_notation(x,y) for x, y in self.board.queens])
-        print(" Pozycja pionka:", self.board.chess_notation(*self.board.pawn_positions))
-
-
-
     def run(self):
-        """Uruchamia grę - główna pętla"""
+        """Główna pętla gry"""
         while True:
-            self.display_current_state()
-            self.check_pawn_threat()
-
-            choice = self.get_user_choice()
+            self._display_current_state()
+            choice = self._get_user_choice()
 
             if choice == 1:
-                self.move_pawn()
+                self._move_pawn()
             elif choice == 2:
-                self.remove_queen()
+                self._remove_queen()
             elif choice == 3:
-                self.check_pawn_threat()
+                self._check_threats()
             elif choice == 4:
-                print(" Dziękuję za grę!")
+                self._exit_game()
                 break
 
 
 
+    def _display_current_state(self):
+        """Wyświetla aktualny stan planszy"""
+        print("\n" + "=" * 40)
+        print("AKTUALNA PLANSZA:")
+        self.display.display_board(self.board.board, self.board.queens, self.board.pawn_positions)
+        print("\nPozycje hetmanów:", [self.board.chess_notation(x, y) for x, y in self.board.queens])
+        print("Pozycja pionka:", self.board.chess_notation(*self.board.pawn_positions))
+        ChessLogic.check_threats(self.board)
 
 
 
+    def _get_user_choice(self):
+        """Pobiera i waliduje wybór użytkownika"""
+        while True:
+            print("\nOpcje:")
+            print("1. Wylosuj nową pozycję dla pionka")
+            print("2. Usuń hetmana")
+            print("3. Sprawdź ponownie bicie")
+            print("4. Zakończ grę")
+
+            try:
+                choice = int(input("Twój wybór (1-4): "))
+                if 1 <= choice <= 4:
+                    return choice
+                print("Nieprawidłowy wybór. Wprowadź liczbę od 1 do 4.")
+            except ValueError:
+                print("Proszę wprowadzić liczbę.")
 
 
 
+    def _move_pawn(self):
+        """Obsługa zmiany pozycji pionka"""
+        old_x, old_y = self.board.pawn_positions
+        self.board.board[old_x][old_y] = ' '
+        self.board.add_random_pawn()
+        print(f"\nNowa pozycja pionka: {self.board.chess_notation(*self.board.pawn_positions)}")
 
 
+
+    def _remove_queen(self):
+        """Obsługa usuwania hetmana"""
+        if not self.board.queens:
+            print("\nBrak hetmanów do usunięcia!")
+            return
+
+        print("\nDostępne hetmany:")
+        for i, (x, y) in enumerate(self.board.queens, 1):
+            print(f"{i}. {self.board.chess_notation(x, y)}")
+
+        try:
+            choice = int(input("Wybierz numer hetmana (0 aby anulować): "))
+            if choice == 0:
+                return
+            if 1 <= choice <= len(self.board.queens):
+                x, y = self.board.queens.pop(choice - 1)
+                self.board.board[x][y] = ' '
+                print(f"Usunięto hetmana na pozycji {self.board.chess_notation(x, y)}")
+            else:
+                print("Nieprawidłowy numer hetmana!")
+        except ValueError:
+            print("Wprowadź poprawny numer!")
+
+
+
+    def _check_threats(self):
+        """Wymusza ponowne sprawdzenie zagrożeń"""
+        ChessLogic.check_threats(self.board)
+
+
+
+    def _exit_game(self):
+        """Zamykanie gry z podsumowaniem"""
+        print("\nDziękujemy za grę!")
+        print("Końcowy stan planszy:")
+        self._display_current_state()
--- zadanie2-hetman_i_pionek/main.py
+++ zadanie2-hetman_i_pionek/main.py
@@ -1,48 +1,6 @@
-from generate_board import GenerateBoard
 from chess_logic import ChessLogic
-from display_board import DisplayBoard
 from game_controller import GameController
 
-import random
-
-
-def get_random_queens_number():
-    """Funkcja do losowania liczby hetmanów"""
-    return random.randint(1, 5)
-
-
-
-# def check_if_queens_threaten_pawn(board):
-#     """Funkcja sprawdzająca czy hetmany grożą pionkowi i wyświetla ich pozycje"""
-#     threading_queens = ChessLogic.find_threatening_queens(board.queens, board.pawn_positions)
-#
-#     if threading_queens:
-#         print("\n Pionek może być zbity przez:")
-#         for queen in threading_queens:
-#             print(f" - {board.chess_notation(*queen)}")
-#     else:
-#         print("\n Pionek jest bezpiczny")
-
-
-
 if __name__ == "__main__":
-    """ Funkcja główna - uruchamia grę """
-    # board = GenerateBoard()
-    # display_board = DisplayBoard()
     game = GameController()
-
-    #
-    # queens_number = get_random_queens_number()
-    #
-    # board.add_random_queen(queens_number)
-    # board.add_random_pawn()
-    #
-    # print("\n Plansza")
-    # display_board.display_board(board.board, board.queens, board.pawn_positions)
-    #
-    # print("\n Pozycje hetmanów:", [board.chess_notation(x,y) for x, y in board.queens])
-    # print(" Pozycja pionka:", board.chess_notation(*board.pawn_positions))
-    #
-    # check_if_queens_threaten_pawn(board)
-
-    game.run()
+    game.start_game()
