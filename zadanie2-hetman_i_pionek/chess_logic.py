85c5131afc0aaa46c073c9dfbde0e222362f055c
Agnieszzka24 <124368425+Agnieszzka24@users.noreply.github.com>
2025-05-05 18:27:19 +0200

    fix: better main functionality

--- zadanie2-hetman_i_pionek/chess_logic.py
+++ zadanie2-hetman_i_pionek/chess_logic.py
@@ -6,24 +6,26 @@ class ChessLogic:
         px, py = pawn_pos
 
         # To samo pole
-        if qx == px and qy == py:
+        if (qx, qy) == (px, py):
             return False
 
-        # Ta sama kolumna lub wiersz
-        if qx == px or qy == py:
-            return True
-
-        # Ta sama przekątna
-        if abs(qx - px) == abs(qy - py):
-            return True
-
-        return False
+        # Ta sama kolumna, wiersz lub przekątna
+        return qx == px or qy == py or abs(qx - px) == abs(qy - py)
 
     @classmethod
     def find_threatening_queens(cls, queens, pawn_pos):
         """Znajduje wszystkie hetmany grożące pionkowi."""
-        threatening = []
-        for queen in queens:
-            if cls.is_queen_threatening_pawn(queen, pawn_pos):
-                threatening.append(queen)
-        return threatening
+        return [queen for queen in queens
+                if cls.is_queen_threatening_pawn(queen, pawn_pos)]
+
+    @classmethod
+    def check_threats(cls, board):
+        """Sprawdza i wyświetla zagrożenia pionka."""
+        threats = cls.find_threatening_queens(board.queens, board.pawn_positions)
+
+        if threats:
+            print("\nPionek może być zbity przez:")
+            for queen in threats:
+                print(f" - {board.chess_notation(*queen)}")
+        else:
+            print("\nPionek jest bezpieczny")
--- zadanie2-hetman_i_pionek/main.py
+++ zadanie2-hetman_i_pionek/main.py
@@ -12,37 +12,37 @@ def get_random_queens_number():
 
 
 
-def check_if_queens_threaten_pawn(board):
-    """Funkcja sprawdzająca czy hetmany grożą pionkowi i wyświetla ich pozycje"""
-    threading_queens = ChessLogic.find_threatening_queens(board.queens, board.pawn_positions)
-
-    if threading_queens:
-        print("\n Pionek może być zbity przez:")
-        for queen in threading_queens:
-            print(f" - {board.chess_notation(*queen)}")
-    else:
-        print("\n Pionek jest bezpiczny")
+# def check_if_queens_threaten_pawn(board):
+#     """Funkcja sprawdzająca czy hetmany grożą pionkowi i wyświetla ich pozycje"""
+#     threading_queens = ChessLogic.find_threatening_queens(board.queens, board.pawn_positions)
+#
+#     if threading_queens:
+#         print("\n Pionek może być zbity przez:")
+#         for queen in threading_queens:
+#             print(f" - {board.chess_notation(*queen)}")
+#     else:
+#         print("\n Pionek jest bezpiczny")
 
 
 
 if __name__ == "__main__":
-    # Wyświelenie planszy
-    board = GenerateBoard()
-    display_board = DisplayBoard()
+    """ Funkcja główna - uruchamia grę """
+    # board = GenerateBoard()
+    # display_board = DisplayBoard()
     game = GameController()
 
-
-    queens_number = get_random_queens_number()
-
-    board.add_random_queen(queens_number)
-    board.add_random_pawn()
-
-    print("\n Plansza")
-    display_board.display_board(board.board, board.queens, board.pawn_positions)
-
-    print("\n Pozycje hetmanów:", [board.chess_notation(x,y) for x, y in board.queens])
-    print(" Pozycja pionka:", board.chess_notation(*board.pawn_positions))
-
-    check_if_queens_threaten_pawn(board)
+    #
+    # queens_number = get_random_queens_number()
+    #
+    # board.add_random_queen(queens_number)
+    # board.add_random_pawn()
+    #
+    # print("\n Plansza")
+    # display_board.display_board(board.board, board.queens, board.pawn_positions)
+    #
+    # print("\n Pozycje hetmanów:", [board.chess_notation(x,y) for x, y in board.queens])
+    # print(" Pozycja pionka:", board.chess_notation(*board.pawn_positions))
+    #
+    # check_if_queens_threaten_pawn(board)
 
     game.run()
